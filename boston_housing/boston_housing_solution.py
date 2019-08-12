94bb20af24a1b93aff14ca316a468b3db998927b
HouariZegai <houarizegai14@gmail.com>
2019-08-12 11:44:45 +0100

    Boston Housing - finish building some supervised regression models

--- boston_housing/boston_housing_solution.py
+++ boston_housing/boston_housing_solution.py
@@ -1,4 +1,4 @@
-""" Please inlude the loading, preproessing and the the regression model. """
+""" Please inlude the loading, preprocessing and the the regression model. """
 
 # import libraries
 import numpy as np
@@ -27,6 +27,35 @@ X = scaler.fit_transform(X)
 
 # split data (train/test)
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, random_state = 11) 
+x_train, x_test, y_train, y_true = train_test_split(X, y, test_size = 0.3, random_state = 11) 
+
+from sklearn.metrics import r2_score
+def evaluate(reg):
+    y_pred = reg.predict(x_test)    
+    print('score of {} are: Accuracy = {}'.format(type(reg).__name__, r2_score(y_true, y_pred)))
 
 # train task
+from sklearn.linear_model import LinearRegression
+lr = LinearRegression()
+lr.fit(x_train, y_train)
+evaluate(lr) # 0.6396268709335121
+
+from sklearn.neighbors import KNeighborsRegressor
+knr = KNeighborsRegressor()
+knr.fit(x_train, y_train)
+evaluate(knr) # 0.7597179673507334
+
+from sklearn.svm import SVR
+svm_svr = SVR()
+svm_svr.fit(x_train, y_train)
+evaluate(svm_svr) # -0.012758120956391172
+
+from sklearn.ensemble import RandomForestRegressor
+rf_reg = RandomForestRegressor()
+rf_reg.fit(x_train, y_train)
+evaluate(rf_reg) # 0.7489899034319791
+
+from sklearn.tree import DecisionTreeRegressor
+dt_reg = DecisionTreeRegressor()
+dt_reg.fit(x_train, y_train)
+evaluate(dt_reg) # 0.6092293932523827
